35e63c230d2f50760f92fc41be0d5f71e24ad70a
Lee sanha <bluegisa@likelion.org>
2018-06-14 06:34:53 +0000

    f

---
+++ .bash_history
@@ -0,0 +1,11 @@
+ls
+rake g model report
+ls
+rake g model report
+rails g model report
+rake db:migrate
+rake db:migraate
+rake db:migrate
+rake db:drop
+rake db:migrate
+d
---
+++ workspace/db/migrate/20180515122047_create_posts.rb
@@ -0,0 +1,18 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :content
+      # default는 사진을 올리지 않고, 글만 썼을 때 오류나지 않게 하는 것.
+      t.string :image, default: ""
+      t.integer :user_id
+      t.integer :report_id
+      t.string :category
+      t.date :finishDate
+
+      t.timestamps null: false
+    end
+  end
+end
+
+# rake db:drop db:migrate로 치면 된다.
